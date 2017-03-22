c8e42cd4eb6507b3fa9a1997459c09a666ef12f2
Andruxin52rus <sasonov9@mail.ru>
2017-03-22 18:56:55 +0300

    Update test2.m

--- Andrew/main/ver2/test2.m
+++ Andrew/main/ver2/test2.m
@@ -1,5 +1,5 @@
-% 1-ray channel with 'angle' and 'd' have been defined the way that turning 
-% input '1' to '1i' to check if result is correct. Test is passed 
+% 1-ray channel with 'angle' and 'd' have been defined the way that turns 
+% input '1' to '1i' to check if the result is correct. Test has been passed 
 
 input = [1]
 antennas_signals = compute_signal_through_channel(input, 2.4e9, 10, 1, 1, 2, 0.03125, 1, 100, [pi / 2], [0], [10]);
